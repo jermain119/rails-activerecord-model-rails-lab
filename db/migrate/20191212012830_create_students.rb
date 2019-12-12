b47fb92bf8dcdc8c5bdfd630ba282d9d89a34c8d
jermain119 <jermain119@gmail.com>
2019-12-11 20:42:53 -0600

    Done.

--- db/migrate/20191212012830_create_students.rb
+++ db/migrate/20191212012830_create_students.rb
@@ -3,6 +3,7 @@ class CreateStudents < ActiveRecord::Migration[5.0]
     create_table :students do |t|
       t.string :first_name
       t.string :last_name
+      t.timestamps null: false
     end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -13,8 +13,6 @@
 ActiveRecord::Schema.define(version: 20191212012830) do
 
   create_table "students", force: :cascade do |t|
-    t.string "first_name"
-    t.string "last_name"
   end
 
 end
